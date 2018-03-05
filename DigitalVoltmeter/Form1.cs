01e0def3126e37e50eb3d53294113351dc606697
chekh <chekhovskikh@mail.ru>
2018-03-06 00:15:48 +0400

    Add Formules

--- DigitalVoltmeter/Form1.cs
+++ DigitalVoltmeter/Form1.cs
@@ -21,8 +21,9 @@ namespace DigitalVoltmeter
             long[] e = processor.GetElementsFromSingleCodes(singleCodes);
             long[] b = processor.GetAllBFromE(e);
             long[] a = processor.GetA(b);
-            for (int i = 0; i < a.Length; i++)
-                textBox1.Text += Convert.ToString(a[i], 2) + Environment.NewLine;
+            string[] formules = processor.Formules(b);
+            for (int i = 0; i < formules.Length; i++)
+                textBox1.Text += formules[i] + Environment.NewLine;
         }
     }
 }
