f533e4e349f1ce21ab95915a692137662cb7003d
quastion <kalugavvlad@gmail.com>
2018-03-06 21:00:10 +0400

    Excel performance added

--- DigitalVoltmeter/Form1.cs
+++ DigitalVoltmeter/Form1.cs
@@ -8,21 +8,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Excel = Microsoft.Office.Interop.Excel;
+
 namespace DigitalVoltmeter
 {
     public partial class Form1 : Form
     {
+        private MathProcessor processor;
+        private long[] singleCodes;
+        private long[] b;
+        private int bitsCount = 32;
+
         public Form1()
         {
             InitializeComponent();
-            MathProcessor processor = new MathProcessor();
-            long[] singleCodes = processor.SingleCodes(8);
+            processor = new MathProcessor();
+            singleCodes = processor.SingleCodes(bitsCount);
             long[] e = processor.GetElementsFromSingleCodes(singleCodes);
-            long[] b = processor.GetAllBFromE(e);
+            b = processor.GetAllBFromE(e);
             long[] a = processor.GetA(b);
             string[] formules = processor.Formules(b);
             for (int i = 0; i < formules.Length; i++)
                 textBox1.Text += formules[i] + Environment.NewLine;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] singleCodesString = singleCodes.Select(a =>  processor.PrettyPrintBits(a, bitsCount)).ToArray();
+            string[] bString = b.Select(a =>  processor.PrettyPrintBits(a, bitsCount)).ToArray();
+            string[] BinaryCodes = new string[bitsCount];
+            for(int i =0; i < BinaryCodes.Length; i++)
+                BinaryCodes[i] = processor.PrettyPrintBits(i, (int)Math.Ceiling(Math.Log(bitsCount, 2)));
+            ExcelTools.GenerateExcel(singleCodesString, bString, BinaryCodes);
         }
     }
 }
